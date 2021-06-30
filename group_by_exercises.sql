a4d5c7c323a94eefa4edd329fb3ff7665a159cd1
hrodjr <hector.rodriguez3@gmail.com>
2021-06-29 19:42:24 -0500

    Update group by exercises with completed work

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -27,22 +27,31 @@ WHERE last_name LIKE '%q%'
 	AND last_name NOT LIKE '%qu%';
 
 -- 6. Add a COUNT() to your results (the query above) to find the number of employees with the same last name. (547)
-SELECT DISTINCT Count(last_name)
+SELECT DISTINCT last_name,
+	COUNT(*) last_name
 FROM employees
 WHERE last_name LIKE '%q%'
-	AND last_name NOT LIKE '%qu%';
+	AND last_name NOT LIKE '%qu%'
+	GROUP BY last_name;
 
 -- 7. Find all all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names. (M - 441 & F - 268)
-SELECT COUNT(*), gender
+SELECT first_name,
+		gender,
+		COUNT(*)
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-GROUP BY gender;
+GROUP BY first_name, gender;
 
 
 -- 8. Using your query that generates a username for all of the employees, generate a count employees for each unique username. Are there any duplicate usernames? BONUS: How many duplicate usernames are there?
-SELECT lower(concat(substr(first_name, 1, 1), substr(last_name,1,4), "_", substr(birth_date,6,2), substr(birth_date,3,2))) as User_Name,
-	first_name,
-	last_name,
-	birth_date
+SELECT LOWER(concat(substr(first_name, 1, 1), substr(last_name,1,4), "_", substr(birth_date,6,2), substr(birth_date,3,2))) AS user_name,
+	COUNT(*) AS unique_username_count
+FROM employees
+GROUP BY user_name
+ORDER BY unique_username_count;
+
+-- 8. Bonus
+SELECT (COUNT(LOWER(concat(substr(first_name, 1, 1), substr(last_name,1,4), "_", substr(birth_date,6,2), substr(birth_date,3,2))))) - (COUNT(DISTINCT(LOWER(concat(substr(first_name, 1, 1), substr(last_name,1,4), "_", substr(birth_date,6,2), substr(birth_date,3,2)))))) as duplicate_user_name	
 FROM employees;
+
 	
